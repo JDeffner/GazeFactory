904a7e03b79b45052bdc49cf110efe58f8025f63
JoelDeffner <134447802+JDeffner@users.noreply.github.com>
2023-05-28 15:58:39 +0200

    Basic Collider System Implemented

--- UnityGazeFactory/Assets/PlayerController.cs
+++ UnityGazeFactory/Assets/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     public SteamVR_Action_Vector2 inputMovementVector;
     public float speed = 1;
+    public Transform vrCameraTransform;
     private CharacterController characterController;
 
     // Start is called before the first frame update
@@ -17,8 +18,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if test to prevent unexpected input value (e.g. teleporting)
-        if(inputMovementVector.axis.magnitude > 0.1f) {
+            characterController.center = new Vector3(vrCameraTransform.localPosition.x, characterController.center.y, vrCameraTransform.localPosition.z);
+
             Vector3 trueDirectionVector = Player.instance.hmdTransform.TransformDirection(new Vector3(inputMovementVector.axis.x, 0, inputMovementVector.axis.y));
             Vector3 gravityVector = new Vector3(0, 9.81f,0);
 
@@ -26,7 +27,11 @@ public class PlayerController : MonoBehaviour
             characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(trueDirectionVector,Vector3.up) - gravityVector * Time.deltaTime);
 
             // transform based movement
-            // transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(trueDirectionVector,Vector3.up - gravityVector * Time.deltaTime);
+            // transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(trueDirectionVector,Vector3.up);
+
+        const float MINIMAL_RECOGNIZED_INPUT = 0.1f;
+        if(inputMovementVector.axis.magnitude > MINIMAL_RECOGNIZED_INPUT) {
+
         }
         
 
