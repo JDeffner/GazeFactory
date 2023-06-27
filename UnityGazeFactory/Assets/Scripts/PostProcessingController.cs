ed59e5a5941d28f44fb3f0365ad4dd5767c647b7
Christof Treitges <117974583+CTreitges@users.noreply.github.com>
2023-06-28 01:18:58 +0200

    added OnObjectCheck

--- UnityGazeFactory/Assets/Scripts/PostProcessingController.cs
+++ UnityGazeFactory/Assets/Scripts/PostProcessingController.cs
@@ -1,18 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
-using Valve.VR;
 
 public class PostProcessingController : MonoBehaviour
 {
+    // Vignette
     public PostProcessVolume postProcessVolume;
     private Vignette vignette;
     public Transform objectToCheck;
+    public GameObject targetedObject;
     public Transform vrCamera;
     public float blinkInterval = 0.5f; 
     private float timer = 0f; 
     public bool isBlinking = true;
     public bool isActive = false;
-
+    private bool isOnObject = false;
+    public List<GameObject> targetedObjects;
+    public List<float> VisionAngle;
+    public bool DebugMode = false;
+    
     private void Start()
     {
         postProcessVolume.profile.TryGetSettings(out vignette);
@@ -20,8 +26,8 @@ public class PostProcessingController : MonoBehaviour
 
     private void Update()
     {
-        GetAngle();
-        if(isBlinking && isActive) {
+        CheckAngle();
+        if(isBlinking && isActive && !isOnObject) {
             timer += Time.deltaTime;
 
             if (timer >= blinkInterval)
@@ -38,6 +44,7 @@ public class PostProcessingController : MonoBehaviour
                 timer = 0f;
             }
         } else {
+            if (isOnObject) vignette.intensity.value = 0f;
             if(!isActive) vignette.intensity.value = 0f;
         }
     }
@@ -54,22 +61,20 @@ public class PostProcessingController : MonoBehaviour
 
     public void SetLeft() {
         vignette.center.value = new Vector2(0.95f, vignette.center.value.y);
-        /// Just for Test Use
-        Debug.Log("setLeft");
+        if(DebugMode) Debug.Log("setLeft");
     }
 
     private void SetRight() {
         vignette.center.value = new Vector2(-0.05f, vignette.center.value.y);
-        /// Just for Test Use
-        Debug.Log("setRight");
+        if(DebugMode) Debug.Log("setRight");
     }
 
-    public void GetAngle() {
+    public void CheckAngle() {
         // Zeichne eine Linie auf der Mitte der VRCamera
         Vector3 cameraPosition = vrCamera.position;
         Vector3 cameraForward = vrCamera.forward;
         Vector3 lineEndPosition = cameraPosition + cameraForward * 10f; // Länge der Linie (10f) anpassen, falls erforderlich
-        Debug.DrawLine(cameraPosition, lineEndPosition, Color.red);
+        if(DebugMode) Debug.DrawLine(cameraPosition, lineEndPosition, Color.red);
         
         // Berechne den Winkel zwischen der Linie und dem Zielobjekt
         Vector3 objectPosition = objectToCheck.position;
@@ -77,17 +82,35 @@ public class PostProcessingController : MonoBehaviour
         Vector2 cameraForwardDirection = new Vector2(cameraForward.x, cameraForward.z);
         float angle = Vector2.SignedAngle(cameraForwardDirection, cameraToObjDirection);
         
-        // Gib den berechneten Winkel aus
-        Debug.Log("Winkel zum Objekt: " + angle);
+        if(DebugMode) Debug.Log("Winkel zum Objekt: " + angle);
         if (angle < 0)
         {
             SetRight();
-            Debug.Log("SetRight");
+            if(DebugMode) Debug.Log("SetRight");
         }
         else
         {
             SetLeft();
-            Debug.Log("SetLeft");
+            if(DebugMode) Debug.Log("SetLeft");
+        }
+        CheckVision(angle);
+    }
+
+    public void CheckVision(float angle)
+    {
+        if (targetedObjects != null && targetedObjects.Count > 0)
+        {
+            int currentIndex = targetedObjects.IndexOf(targetedObject);
+            if (angle < VisionAngle[currentIndex] && angle > VisionAngle[currentIndex] * (-1))
+            {
+                isOnObject = true;
+                if(DebugMode) Debug.Log("IsOnObject");
+            }
+            else
+            {
+                isOnObject = false;
+                if(DebugMode) Debug.Log("IsNotOnObject");
+            }
         }
     }
 }
