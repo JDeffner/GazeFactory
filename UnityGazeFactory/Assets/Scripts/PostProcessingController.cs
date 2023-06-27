879b0db64716bec1dbc836dcab00e76059b1238c
Christof Treitges <117974583+CTreitges@users.noreply.github.com>
2023-06-27 12:52:10 +0200

    Added Direction Check

--- UnityGazeFactory/Assets/Scripts/PostProcessingController.cs
+++ UnityGazeFactory/Assets/Scripts/PostProcessingController.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using Valve.VR;
 
 public class PostProcessingController : MonoBehaviour
 {
     public PostProcessVolume postProcessVolume;
     private Vignette vignette;
-
+    public Transform objectToCheck;
+    public Transform vrCamera;
     public float blinkInterval = 0.5f; 
     private float timer = 0f; 
     public bool isBlinking = true;
@@ -18,6 +20,7 @@ public class PostProcessingController : MonoBehaviour
 
     private void Update()
     {
+        GetAngle();
         if(isBlinking && isActive) {
             timer += Time.deltaTime;
 
@@ -39,34 +42,52 @@ public class PostProcessingController : MonoBehaviour
         }
     }
 
-    public void setActive() {
+    public void SetActive() {
         isActive = true;
         vignette.intensity.value = 0.4f;
     }
 
-    public void setInActive() {
+    public void SetInActive() {
         isActive = false;
         vignette.intensity.value = 0f;
     }
 
-    public void setLeft() {
+    public void SetLeft() {
         vignette.center.value = new Vector2(0.95f, vignette.center.value.y);
         /// Just for Test Use
         Debug.Log("setLeft");
     }
 
-    public void setRight() {
+    private void SetRight() {
         vignette.center.value = new Vector2(-0.05f, vignette.center.value.y);
         /// Just for Test Use
         Debug.Log("setRight");
     }
 
-    /// Just for Test Use
-    public void changeIsBlinking() {
-        if(isBlinking) {
-            isBlinking = false;
-        } else {
-            isBlinking = true;
+    public void GetAngle() {
+        // Zeichne eine Linie auf der Mitte der VRCamera
+        Vector3 cameraPosition = vrCamera.position;
+        Vector3 cameraForward = vrCamera.forward;
+        Vector3 lineEndPosition = cameraPosition + cameraForward * 10f; // Länge der Linie (10f) anpassen, falls erforderlich
+        Debug.DrawLine(cameraPosition, lineEndPosition, Color.red);
+        
+        // Berechne den Winkel zwischen der Linie und dem Zielobjekt
+        Vector3 objectPosition = objectToCheck.position;
+        Vector2 cameraToObjDirection = new Vector2(objectPosition.x - cameraPosition.x, objectPosition.z - cameraPosition.z);
+        Vector2 cameraForwardDirection = new Vector2(cameraForward.x, cameraForward.z);
+        float angle = Vector2.SignedAngle(cameraForwardDirection, cameraToObjDirection);
+        
+        // Gib den berechneten Winkel aus
+        Debug.Log("Winkel zum Objekt: " + angle);
+        if (angle < 0)
+        {
+            SetRight();
+            Debug.Log("SetRight");
+        }
+        else
+        {
+            SetLeft();
+            Debug.Log("SetLeft");
         }
     }
 }
