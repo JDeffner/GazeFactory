10901ac4237b22fd963f45e5f75b5e4947174568
Christof Treitges <117974583+CTreitges@users.noreply.github.com>
2023-07-08 17:01:13 +0200

    added TextGazeGuing

---
+++ UnityGazeFactory/Assets/Scripts/Behaviours/BeispielBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeispielBehaviour : StateMachineBehaviour
+{
+    private GameObject targetedObject;
+    private SimpleGazeMark gazeMark;
+    private PostProcessingController postController;
+    private SimpleGazeText gazeText;
+    
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+    {
+        // Set targeted Object
+        targetedObject = GameObject.Find("CPRPMUp"); 
+        // Set Text and TextColor
+        gazeText.text = "Test \n ist \n gelungen!!!!"; // "\n" für Zeilenumbruch
+        gazeText.textColor = "#61A5FF"; // Light Blue Hex Code
+        // Find GazeGuiding Components
+        gazeMark =  FindObjectOfType<SimpleGazeMark>();
+        postController = FindObjectOfType<PostProcessingController>();
+        gazeText = FindObjectOfType<SimpleGazeText>();
+        // Change Targeted Objects
+        gazeMark.targetedObject = targetedObject;
+        postController.targetedObject = targetedObject;
+        gazeText.targetedObject = targetedObject;
+        // Set GazeGuiding active
+        gazeMark.isActive = true;
+        postController.isActive = true;
+        gazeText.isActive = true;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+    {
+        // gazeMark.isActive = false;
+    }
+}
