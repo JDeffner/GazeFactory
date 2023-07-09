a297b8e87560b1045153e72a90cb576eb731789c
1Houston1 <92863010+1Houston1@users.noreply.github.com>
2023-07-09 22:49:20 +0200

    reworked SOP s3

---
+++ UnityGazeFactory/Assets/animateExtractRods.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class animateExtractRods : StateMachineBehaviour
+{
+    private GameObject targetedObject;
+    private SimpleGazeMark gazeMark;
+    private PostProcessingController postController;
+    
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+    {
+        // Set targeted Object
+        targetedObject = GameObject.Find("ExtractRodsButton"); 
+        // Find GazeGuiding Components
+        gazeMark =  FindObjectOfType<SimpleGazeMark>();
+        postController = FindObjectOfType<PostProcessingController>();
+        // Change Targeted Objects
+        gazeMark.targetedObject = targetedObject;
+        postController.targetedObject = targetedObject;
+        // Set GazeGuiding active
+        gazeMark.isActive = true;
+        postController.isActive = true;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+    {
+        // gazeMark.isActive = false;
+    }
+}
