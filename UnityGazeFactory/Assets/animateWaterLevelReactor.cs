603c6ff27ee6753f1bb89384550b9d95a2e97e47
1Houston1 <92863010+1Houston1@users.noreply.github.com>
2023-07-11 22:42:36 +0200

    added SOP s6 and s7

---
+++ UnityGazeFactory/Assets/animateWaterLevelReactor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class animateWaterLevelReactor : StateMachineBehaviour
+{
+    private GameObject targetedObject;
+    private SimpleGazeMark gazeMark;
+    private PostProcessingController postController;
+    
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+    {
+        // Set targeted Object
+        targetedObject = GameObject.Find("TextWaterLevelReactor (2)"); 
+        // Find GazeGuiding Components
+        gazeMark =  FindObjectOfType<SimpleGazeMark>();
+        postController = FindObjectOfType<PostProcessingController>();
+        // Change Targeted Objects
+        gazeMark.targetedObject = targetedObject;
+        postController.targetedObject = targetedObject;
+        // Set GazeGuiding active
+        gazeMark.isActive = true;
+        postController.isActive = true;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+    {
+        // gazeMark.isActive = false;
+    }
+}
---
+++ UnityGazeFactory/Assets/controllingWP1accordingToRods.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.ProBuilder;
+
+namespace DefaultNamespace
+{
+    public class controllingWP1accordingToRods : MonoBehaviour
+    {
+        private bool moin = true;
+        private int var1 = 2000;
+        private int tmp = 2000;
+        private float delay = 0.3f;
+        private float counter = 0f;
+        public bool isSinking = false;
+        public int dCalc = 0;
+        private int i = 0;
+        public void Update()
+        {
+            counter += Time.deltaTime;
+            if (counter >= delay)
+            {
+                tmp = var1;
+                var1 = ControllerCubeBehaviour.nppSystemInterface.getWaterLevelReactor();
+
+                if (tmp - var1 > 0)
+                {
+                    Debug.Log("sinking");
+                    isSinking = true;
+                }
+                else
+                {
+                    Debug.Log("rising");
+                    isSinking = false;
+                }
+
+                i = tmp - var1;
+                if (i < 0)
+                {
+                    dCalc = i * (-1);
+                }
+                else
+                {
+                    dCalc = i;
+                }
+                counter = 0f;
+                Debug.Log("Reichweite: " + dCalc);
+            }
+        }
+    }
+}
